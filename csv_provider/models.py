cd2696f6e7bc9bcefa64ec61a128c6238f1cc607
y-du <42994541+y-du@users.noreply.github.com>
2021-05-19 16:18:49 +0200

    init

---
+++ csv_provider/models.py
@@ -0,0 +1,54 @@
+"""
+   Copyright 2021 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
+import typing
+import simple_struct
+
+
+__all__ = ("Job", "JobStatus", "DataItem")
+
+
+class JobStatus:
+    pending = "pending"
+    running = "running"
+    finished = "finished"
+    failed = "failed"
+    aborted = "aborted"
+
+
+@simple_struct.structure
+class Job:
+    id = None
+    created = None
+    status = JobStatus.pending
+    source_id = None
+    reason = None
+
+
+@simple_struct.structure
+class DataItem:
+    source_id: str = None          # required
+    time_field: str = None          # required
+    delimiter: str = None           # required
+    sources: dict = None
+    size: int = 0
+    created: str = None
+    columns: list = None
+    default_values: dict = None
+    file: str = None
+    checksum = None
+    compressed = None
