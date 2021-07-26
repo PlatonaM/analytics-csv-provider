ed9c9c53492f57ac8fbbb4e28508ebcc64afd1ec
y-du <42994541+y-du@users.noreply.github.com>
2021-07-26 15:46:38 +0200

    catch exception after db migration

--- csv_provider/handlers/jobs.py
+++ csv_provider/handlers/jobs.py
@@ -65,11 +65,14 @@ class Worker(multiprocessing.Process):
             old_files = self.__data_item.files
             result_obj.data_item = self.__data_handler.create(self.__data_item.source_id, self.__data_item.time_field, self.__data_item.delimiter)
             if old_files:
-                for old_file in old_files:
-                    try:
-                        self.__data_handler.remove(old_file)
-                    except Exception as ex:
-                        logger.warning("{}: could not remove old file - {}".format(self.__job.id, ex))
+                try:
+                    for old_file in old_files:
+                        try:
+                            self.__data_handler.remove(old_file)
+                        except Exception as ex:
+                            logger.warning("{}: could not remove old file '{}' - {}".format(self.__job.id, old_file, ex))
+                except Exception as ex:
+                    logger.warning("{}: could not remove old files - {}".format(self.__job.id, ex))
             self.__job.status = models.JobStatus.finished
             logger.debug("{}: completed successfully".format(self.__job.id))
         except Exception as ex:
